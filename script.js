5e6596224063b8c4d4d11658956206bf88646525
Everettex <96892745+everettexue@users.noreply.github.com>
2025-06-10 22:14:27 +0000

    test2

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-gsap.to(".box", { 
-  x: 200,
-});
+gsap
+  .timeline({
+    defaults: { duration: 2, ease: "expo.inOut" },
+    repeat: -1
+  })
+  .to("#shape1", { morphSVG: "#shape2" })
+  .to("#shape1", { morphSVG: "#shape3" })
+  .to("#shape1", { morphSVG: "#shape4" })
+  .to("#shape1", { morphSVG: "#shape1" });
