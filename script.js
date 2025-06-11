11be7443d866fe37d6f4b92e2332ce868b717be4
Everettex <96892745+everettexue@users.noreply.github.com>
2025-06-11 21:13:55 +0000

    herthrtherth

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-
-
-
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 let smoother = ScrollSmoother.create({
     wrapper: "#smooth-wrapper",
     content: "#smooth-content",
+    smooth: 1.5 // Increase for more smoothing
 })
