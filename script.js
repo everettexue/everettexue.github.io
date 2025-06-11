7688d43c2dac695c42494af468a47640274b4337
Everettex <96892745+everettexue@users.noreply.github.com>
2025-06-11 20:27:11 +0000

    tset

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollSmoother.min.js"></script>
+
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 let smoother = ScrollSmoother.create({
