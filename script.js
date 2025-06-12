0caf4962c9688e51211197115253e505b1001dab
Everettex <96892745+everettexue@users.noreply.github.com>
2025-06-11 21:10:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ gsap.set("h1", { opacity: 1 });
 let split = SplitText.create("#heading", { type: "chars" });
 //now animate each character into place from 20px below, fading in:
 gsap.from(split.chars, {
-  y: 20,
+  y: 96,
   autoAlpha: 0,
   stagger: 0.05
 });
