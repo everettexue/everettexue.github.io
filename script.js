73c8bf11bc37af7dc4483f92fc0259931e8009c5
Everettex <96892745+everettexue@users.noreply.github.com>
2025-06-12 09:58:23 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,21 @@ window.addEventListener("load", () => {
 });
 
 gsap.set("h1", { opacity: 1 });
-gsap.set("h2", { opacity: 1 });
 
 let split = SplitText.create("#heading", { type: "chars" });
 //now animate each character into place from 20px below, fading in:
 gsap.from(split.chars, {
   y: 56,
+  autoAlpha: 0,
+  stagger: 0.05
+});
+
+gsap.set("h2", { opacity: 1 });
+
+let split = SplitText.create("#title", { type: "chars" });
+//now animate each character into place from 20px below, fading in:
+gsap.from(split.chars, {
+  y: 28,
   autoAlpha: 0,
   stagger: 0.05
 });
