a01987d3fad7567f465865af399e7c94ce7ce22f
Everettex <96892745+everettexue@users.noreply.github.com>
2025-06-13 15:41:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ gsap.from(splitt.chars, {
 gsap.fromTo("#line-under-title",
   { width: "0%", opacity: 0 },
   {
-    width: "100%", // You can tweak this width
+    width: "175%", // You can tweak this width
     opacity: 1,
-    duration: 1.2,
+    duration: 0.8,
     ease: "power2.out",
     scrollTrigger: {
       trigger: "#title",
